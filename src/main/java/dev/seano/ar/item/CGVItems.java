ed98916aee7b41a2bec399cc5f1683c40888f69e
Sean O'Connor <so@seano.dev>
2023-02-15 21:32:18 -0700

    Update CGVItems::register methods

--- src/main/java/dev/seano/ar/item/CGVItems.java
+++ src/main/java/dev/seano/ar/item/CGVItems.java
@@ -2,8 +2,8 @@ package dev.seano.ar.item;
 
 import dev.seano.ar.CGVMod;
 import dev.seano.ar.block.CGVBlocks;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -20,7 +20,8 @@ public class CGVItems {
 
     private static final HashMap<Identifier, Item> ITEMS = new HashMap<>();
 
-    public static final Item ROSE = register(CGVBlocks.ROSE);
+    public static final Item ROSE = register(Registries.BLOCK.getId(CGVBlocks.ROSE)
+            .getPath(), new BlockItem(CGVBlocks.ROSE, new FabricItemSettings()));
 
     public static void registerAll() {
         ITEMS.forEach(((identifier, item) -> Registry.register(Registries.ITEM, identifier, item)));
@@ -29,36 +30,13 @@ public class CGVItems {
                 .register(entries -> entries.add(ROSE));
     }
 
-    @SuppressWarnings("SameParameterValue")
-    private static Item register(Block block) {
-        return register(new BlockItem(block, new Item.Settings()));
+    private static Item register(String path, Item item) {
+        return register(CGVMod.identifier(path), item);
     }
 
-    private static Item register(Block block, Block... blocks) {
-        BlockItem blockItem = new BlockItem(block, new Item.Settings());
-        for (Block block2 : blocks) {
-            Item.BLOCK_ITEMS.put(block2, blockItem);
-        }
-        return register(blockItem);
-    }
-
-    private static Item register(BlockItem item) {
-        return register(item.getBlock(), item);
-    }
-
-    protected static Item register(Block block, Item item) {
-        return register(Registries.BLOCK.getId(block), item);
-    }
-
-    private static Item register(String id, Item item) {
-        return register(CGVMod.identifier(id), item);
-    }
-
-    private static Item register(Identifier id, Item item) {
-        if (item instanceof BlockItem) {
-            ((BlockItem) item).appendBlocks(Item.BLOCK_ITEMS, item);
-        }
-        ITEMS.put(id, item);
+    private static Item register(Identifier identifier, Item item) {
+        if (item instanceof BlockItem) ((BlockItem) item).appendBlocks(Item.BLOCK_ITEMS, item);
+        ITEMS.put(identifier, item);
         return item;
     }
 }
