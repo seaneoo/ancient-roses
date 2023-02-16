056516c8a04b1d59459d91f03c2cf8b171af85e9
Sean O'Connor <so@seano.dev>
2023-02-15 21:33:28 -0700

    Position ROSE item after DANDELION item in creative inventory

--- src/main/java/dev/seano/ar/item/CGVItems.java
+++ src/main/java/dev/seano/ar/item/CGVItems.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
@@ -24,11 +25,12 @@ public class CGVItems {
     public static final Item ROSE = register(Registries.BLOCK.getId(CGVBlocks.ROSE)
             .getPath(), new BlockItem(CGVBlocks.ROSE, new FabricItemSettings().requires(FeatureFlags.UPDATE_1_20)));
 
+    @SuppressWarnings("UnstableApiUsage")
     public static void registerAll() {
         ITEMS.forEach(((identifier, item) -> Registry.register(Registries.ITEM, identifier, item)));
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
-                .register(entries -> entries.add(ROSE));
+                .register(entries -> entries.addAfter(Items.DANDELION, ROSE));
     }
 
     private static Item register(String path, Item item) {
