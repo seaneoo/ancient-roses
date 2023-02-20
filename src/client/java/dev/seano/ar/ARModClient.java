17cc0a8d84a7910ebeae464bfc6435cded6e826b
Sean O'Connor <so@seano.dev>
2023-02-20 09:30:53 -0700

    Add Environment CLIENT flag

--- src/client/java/dev/seano/ar/ARModClient.java
+++ src/client/java/dev/seano/ar/ARModClient.java
@@ -2,9 +2,12 @@ package dev.seano.ar;
 
 import dev.seano.ar.block.ARBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
+@Environment(EnvType.CLIENT)
 public class ARModClient implements ClientModInitializer {
 
     @Override
